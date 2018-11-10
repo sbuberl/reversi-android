7344001ea5e9a23f393514aea35f16b510713627
sbuberl <sbuberl@gmail.com>
2018-11-09 20:19:30 -0500

    Adds opposite function to Stone

--- app/src/main/java/com/sbuberl/reversi/Cell.kt
+++ app/src/main/java/com/sbuberl/reversi/Cell.kt
@@ -3,6 +3,14 @@ package com.sbuberl.reversi
 enum class Stone
 {
     NONE, BLACK, WHITE, HINT;
+    fun opposite() : Stone? {
+        if(this === Stone.BLACK) {
+            return Stone.WHITE
+        } else if(this === Stone.WHITE) {
+            return Stone.BLACK
+        }
+        return null
+    }
 }
 
 data class Cell(val row: Int, val column: Int, var stone :  Stone)
